b0d57264df62af30a1277c76513e6d21ec786a64
zuzannaNa <69386096+zuzannaNa@users.noreply.github.com>
2020-11-07 16:25:36 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+const db = require("./models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+
+require("./routes/item.routes.js")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server is running on por ${PORT}.");
+});
+
+
