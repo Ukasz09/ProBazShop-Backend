f46db6f2beb4a01b44b10830daba5d7694823113
Tomasz <20225776+tomjasinski@users.noreply.github.com>
2020-11-08 22:01:30 +0100

    Added controllers

--- controllers/item.controller.js
+++ controllers/item.controller.js
@@ -1,91 +1,132 @@
 const db = require("../models");
 const Item = db.items;
 
-exports.findAll = (req, res) => {
-    const title = req.query.title;
-    var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
-  
-    Item.find(condition)
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving items. "
-        });
-      });
-  };
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.title) {
+    res.status(400).send({ message: "Content can not be empty!" });
+    return;
+  }
 
-  exports.create = (req, res) => {
-    // Validate request
-    if (!req.body.title) {
-      res.status(400).send({ message: "Content can not be empty!" });
-      return;
-    }
-  
-    // Create an item
-    const item = new Item({
- 
-                title: req.body.title,
-                description: req.body.description,
-                category: req.body.category,
-                price: req.body.price,
-                color: req.body.color,
-                size: req.bodysize,
-                img: req.body.img
-            },
+  // Create an item
+  const item = new Item({
+
+              title: req.body.title,
+              description: req.body.description,
+              category: req.body.category,
+              price: req.body.price,
+              color: req.body.color,
+              size: req.bodysize,
+              img: req.body.img
+          },
 );
-  
-    // Save Item in the database
-    item
-      .save(item)
-      .then(data => {
-        res.send(data);
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while creating the Item."
-        });
-      });
-  };
 
-  exports.update = (req, res) => {
-    if (!req.body) {
-      return res.status(400).send({
-        message: "Data to update can not be empty!"
+  // Save Item in the database
+  item
+    .save(item)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Item."
       });
-    }
-  
-    const id = req.params.id;
-  
-    Item.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-      .then(data => {
-        if (!data) {
-          res.status(404).send({
-            message: `Cannot update Item with id=${id}. Maybe Item was not found!`
-          });
-        } else res.send({ message: "Item was updated successfully." });
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Error updating Item with id=" + id
-        });
-      });
-  };
+    });
+};
 
-  exports.deleteAll = (req, res) => {
-    Item.deleteMany({})
-      .then(data => {
+exports.findAll = (req, res) => {
+  const title = req.query.title;
+  var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
+
+  Item.find(condition)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving items. "
+      });
+    });
+};
+
+// Find an single Item with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Item.findById(id)
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found Item with id " + id });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Item with id=" + id });
+    });
+};
+  
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
+
+  const id = req.params.id;
+
+  Item.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Item with id=${id}. Maybe Item was not found!`
+        });
+      } else res.send({ message: "Item was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Item with id=" + id
+      });
+    });
+};
+
+
+// Delete an Item with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Item.findByIdAndRemove(id)
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Item with id=${id}. Maybe Item was not found!`
+        });
+      } else {
         res.send({
-          message: `${data.deletedCount} Items were deleted successfully!`
+          message: "Item was deleted successfully!"
         });
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while removing all items."
-        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Item with id=" + id
       });
-  };
+    });
+};
+  
+exports.deleteAll = (req, res) => {
+  Item.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} Items were deleted successfully!`
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all items."
+      });
+    });
+};
