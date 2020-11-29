77e02954e974ceb6cb299128d763be2db287f4f1
ukasz09 <246703@student.pwr.edu.pl>
2020-11-29 13:15:59 +0100

    Format item.model

--- models/item.model.js
+++ models/item.model.js
@@ -1,30 +1,26 @@
-module.exports = mongoose => {
-    var schema = mongoose.Schema(
-            {
-                
-                      name: String,
-                      description: String,
-                      imageURL: String,
-                      size: String,
-                      color: String,
-                      price: Number,
-                      starRating: Number,
-                      category: String,
-                      availableQty: Number                   
-                
-            },
-             { timestamps: true }
+module.exports = (mongoose) => {
+  var schema = mongoose.Schema(
+    {
+      name: String,
+      description: String,
+      imageURL: String,
+      size: String,
+      color: String,
+      price: Number,
+      starRating: Number,
+      category: String,
+      availableQty: Number,
+    },
+    { timestamps: true }
+  );
 
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
 
-            )
-    
-    schema.method("toJSON", function() {
-        const { __v, _id, ...object } = this.toObject();
-        object.id = _id;
-        return object;
-      });
-    
-    const Item = mongoose.model("item", schema);
+  const Item = mongoose.model("item", schema);
 
-    return Item;
-}
+  return Item;
+};
